2cd971b9e602e3d2d8d69b0d489c67d0b518f848
fsampson30 <fsampson30@gmail.com>
2024-11-12 07:34:37 -0300

    Initial commit

---
+++ src/main/java/com/sampson/pipelines_ci_cd/PipelinesCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.sampson.pipelines_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinesCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinesCiCdApplication.class, args);
+	}
+
+}
