d3eee626491d9890bc4a4a7feb00e972edef1563
fsampson30 <fsampson30@gmail.com>
2024-11-13 07:42:32 -0300

    Created basic controller.

---
+++ src/main/java/controller/PeopleController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PeopleController {
+
+    @GetMapping("/test")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @RequestMapping ("/list")
+    public List<String> getTestValues(){
+        return List.of("Test1","Test2","Test3","Test4","Test5","Test6");
+    }
+}
