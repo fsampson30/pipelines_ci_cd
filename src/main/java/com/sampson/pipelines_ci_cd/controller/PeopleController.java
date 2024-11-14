507db660512a5f39ecae0f047c3e37bf4621bb3d
fsampson30 <fsampson30@gmail.com>
2024-11-14 07:32:18 -0300

    Refactored package location.

--- src/main/java/com/sampson/pipelines_ci_cd/controller/PeopleController.java
+++ src/main/java/com/sampson/pipelines_ci_cd/controller/PeopleController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.sampson.pipelines_ci_cd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
